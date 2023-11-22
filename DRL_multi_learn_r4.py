33ee50401fa1cf72ea37de9d6324f18a9dc650c7
bill304 <151612596+bill304@users.noreply.github.com>
2023-11-22 15:07:57 +0900

    Update DRL_multi_learn_r4.py
    step 함수 메모리 최적화
    
    train 함수에서 batch에서 샘플 하나 뽑고 training 하는 방식이 아니라 batch 전체를 통째로 training하는 batch training 방식으로 수정
    (stable training)
    
    learning rate가 TTI마다 감소하게 바뀜.
    
    state size 코드 수정

--- DRL_multi_learn_r4.py
+++ DRL_multi_learn_r4.py
@@ -12,24 +12,20 @@ from tensorflow.keras.layers import Input
 from collections import deque
 import time
 from DRL_env import DRLenv
+import gc
 
+from memory_profiler import profile
 
 
 class DRLmultiagent(object):
     def __init__(self, state_size, action_size, action_cand, pmax, noise):
-        self.TTIs = 1000
-        self.simul_rounds = 1
 
-        self.EPSILON = 0.2
-
-        self.EPSILON_DECAY = 1-math.pow(10,-4)
-        #self.EPSILON_DECAY = 0.99
-        self.EPSILON_MIN = 0.01
-
-        self.learning_rate = 5 * math.pow(10, -3)
+        self.initial_learning_rate = 1e-2
+        self.learning_rate = self.initial_learning_rate
+        self.lambda_lr = 1e-4  # decay rate for learning rate
         #self.learning_rate_decay = 1-math.pow(10,-4)
 
-        self.gamma = 0.9
+        self.gamma = 0.5
 
         self.pmax = pmax #38dbm
 
@@ -52,7 +48,7 @@ class DRLmultiagent(object):
 
         self.model = self.build_network
 
-        self.replay_buffer = deque(maxlen=5000)
+        self.replay_buffer = deque(maxlen=self.transmitters*1000)
         self.update_rate = 100
         self.main_network = self.build_network()
         weight = self.main_network.get_weights()
@@ -64,6 +60,12 @@ class DRLmultiagent(object):
         self.loss = []
 
         self.temp_reward1 = 0
+
+    def update_learning_rate(self):
+        self.learning_rate *= (1 - self.lambda_lr)
+        for i in range(1, self.transmitters + 1):
+            getattr(self, f'target_network{i}').optimizer.lr.assign(self.learning_rate)
+        self.main_network.optimizer.lr.assign(self.learning_rate)
 
 
 
@@ -95,6 +97,7 @@ class DRLmultiagent(object):
 
         return action
 
+
     def step(self, state, actions, TTI, max_TTI, channel_gain, next_channel_gain, agent):
 
         if TTI >= max_TTI:
@@ -102,152 +105,76 @@ class DRLmultiagent(object):
         else:
             done = False
 
-        old_state = np.copy(state)
-        next_state = np.zeros([self.state_size])
+        if hasattr(self, 'next_state'):
+            self.next_state.fill(0)
+        else:
+            self.next_state = np.zeros([self.state_size])
 
-        reward = 0
-        self.temp_reward1 = 0
-        temp_reward2 = 0
-
-        action_of_agent = actions[agent]
-        inter = 0
-
-        direct_signal = channel_gain[agent, agent] * action_of_agent
-
-        for j in range(self.transmitters):
-            if j == agent:
-                inter += 0
-            else:
-                action_of_interferer = actions[j]
-                gain_temp_interferer = channel_gain[j, agent]
-                inter_of_interferer = gain_temp_interferer * action_of_interferer
-                inter += inter_of_interferer
+        direct_signal = channel_gain[agent, agent] * actions[agent]
+        inter = np.sum(channel_gain[:, agent] * actions) - direct_signal
 
         self.temp_reward1 = math.log2(1 + direct_signal / (inter + self.noise))
 
+        reward = self.temp_reward1
         for j in range(self.users):
-            inter_of_interfered = 0
-            inter_of_interfered_without_agent = 0
-            if j == agent:
-                temp_reward2 += 0
-            else:
-
-                for k in range(self.transmitters):
-                    if k == j:
-                        inter_of_interfered += 0
-                        inter_of_interfered_without_agent += 0
-                    else:
-                        if k != agent:
-                            action_to_interfered = actions[k]
-                            gain_temp_interferer = channel_gain[k, j]
-                            inter_to_interfered = gain_temp_interferer * action_to_interfered
-                            inter_of_interfered += inter_to_interfered
-                            inter_of_interfered_without_agent += inter_to_interfered
-
-                        else:
-                            action_to_interfered = actions[k]
-                            gain_temp_interferer = channel_gain[k, j]
-                            inter_to_interfered = gain_temp_interferer * action_to_interfered
-                            inter_of_interfered += inter_to_interfered
-                            inter_of_interfered_without_agent += 0
-
-                rate_with_agent = math.log2(1 + (channel_gain[j, j] * actions[j]) / (inter_of_interfered + self.noise))
+            if j != agent:
+                inter_of_interfered = np.sum(channel_gain[:, j] * actions) - channel_gain[j, j] * actions[j]
+                rate_with_agent = math.log2(1 + channel_gain[j, j] * actions[j] / (inter_of_interfered + self.noise))
+                inter_of_interfered_without_agent = inter_of_interfered - channel_gain[agent, j] * actions[agent]
                 rate_without_agent = math.log2(
-                    1 + (channel_gain[j, j] * actions[j]) / (inter_of_interfered_without_agent + self.noise))
-                temp_reward2 += (rate_without_agent - rate_with_agent)
+                    1 + channel_gain[j, j] * actions[j] / (inter_of_interfered_without_agent + self.noise))
+                reward -= (rate_without_agent - rate_with_agent)
 
-        reward = self.temp_reward1 - temp_reward2
 
-        next_state[0] = actions[agent]
-        next_state[1] = self.temp_reward1
-        next_state[2] = next_channel_gain[agent, agent]
-        next_state[3] = channel_gain[agent, agent]
+        self.next_state[0] = actions[agent]
+        self.next_state[1] = self.temp_reward1
+        self.next_state[2] = next_channel_gain[agent, agent]
+        self.next_state[3] = channel_gain[agent, agent]
+        self.next_state[4] = np.sum(next_channel_gain[:, agent] * actions) - next_channel_gain[agent, agent] * actions[agent]
+        self.next_state[5] = state[4]
 
-        new_agent_inter = 0
-        for j in range(self.transmitters):
-            if j == agent:
-                new_agent_inter += 0
-            else:
-                action_of_interferer = actions[j]
-                gain_temp_interferer = next_channel_gain[j, agent]
-                inter_of_interferer = gain_temp_interferer * action_of_interferer
-                new_agent_inter += inter_of_interferer
-        next_state[4] = new_agent_inter
-        next_state[5] = old_state[4]
+        state_index = 6
 
         for j in range(self.transmitters):
             if j != agent:
-                next_state[4*j+6] = next_channel_gain[j, agent] * actions[j]
+                # Calculate interference from other agents
+                # ...
 
-                direct_signal_tmp = channel_gain[j, j] * actions[j]
+                # Update the state array with interferer information
+                self.next_state[state_index] = next_channel_gain[j, agent] * actions[j]
+                self.next_state[state_index + 1] = math.log2(1 + channel_gain[j, j] * actions[j] / (np.sum(channel_gain[:, j] * actions) - channel_gain[j, j] * actions[j] + self.noise))
 
-                inter_tmp = 0
+                self.next_state[state_index + 2] = state[state_index]
 
-                for k in range(self.transmitters):
-                    if k == j:
-                        inter_tmp += 0
-                    else:
-                        action_of_interferer_tmp = actions[k]
-                        gain_temp_interferer_tmp = channel_gain[k, j]
-                        inter_of_interferer_tmp = gain_temp_interferer_tmp * action_of_interferer_tmp
-                        inter_tmp += inter_of_interferer_tmp
+                self.next_state[state_index + 3] = state[state_index+1]
 
-                next_state[4*j + 7] = math.log2(1 + direct_signal_tmp / (inter_tmp + self.noise))
-
-                next_state[4*j + 8] = old_state[4*j+6]
-
-                next_state[4 * j + 9] = old_state[4 * j + 7]
-
-            else:
-                next_state[4 * j + 6] = 0
-                next_state[4 * j + 7] = 0
-                next_state[4 * j + 8] = 0
-                next_state[4 * j + 9] = 0
+                # Move to the next set of indices for the next interferer
+                state_index += 4
 
         for k in range(self.transmitters):
             if k != agent:
-                next_state[3*k+6+4*self.transmitters] = channel_gain[k, k]
 
-                direct_signal_tmp = channel_gain[k, k] * actions[k]
-                inter_tmp = 0
+                self.next_state[state_index] = channel_gain[k, k]
 
-                for m in range(self.transmitters):
-                    if m == k:
-                        inter_tmp += 0
-                    else:
-                        action_of_interferer_tmp = actions[m]
-                        gain_temp_interferer_tmp = channel_gain[m, k]
-                        inter_of_interferer_tmp = gain_temp_interferer_tmp * action_of_interferer_tmp
-                        inter_tmp += inter_of_interferer_tmp
+                self.next_state[state_index + 1] = math.log2(1 + channel_gain[k, k] * actions[k] / (np.sum(channel_gain[:, k] * actions) - channel_gain[k, k] * actions[k] + self.noise))
 
-                next_state[3 * k + 7 + 4*self.transmitters] = math.log2(1 + direct_signal_tmp / (inter_tmp + self.noise))
+                self.next_state[state_index + 2] = (channel_gain[agent, k] * actions[agent]) / (np.sum(channel_gain[:, k] * actions) - channel_gain[k, k] * actions[k] + self.noise)
 
-                next_state[3 * k + 8 + 4 * self.transmitters] = (channel_gain[agent, k] * actions[agent]) / (inter_tmp + self.noise)
+                state_index += 3
 
-            else:
-                next_state[3 * k + 6 + 4 * self.transmitters] = 0
-                next_state[3 * k + 7 + 4 * self.transmitters] = 0
-                next_state[3 * k + 8 + 4 * self.transmitters] = 0
-
-
-
-
-
-        #next_state = array_of_interference#.flatten()
-        #next_state = tf.convert_to_tensor(next_state.reshape(1, -1), dtype=tf.float32)
+        gc.collect()
 
         info = {}
 
-        return next_state, reward, done, info
-
-
-
+        return self.next_state, reward, done, info
 
     def train(self, batch_size):
 
+        self.update_learning_rate()
+
         # compute the Q value using the target network
         minibatch = random.sample(self.replay_buffer, batch_size)
-
+        '''
         for state, action, reward, next_state, done, agent in minibatch:
             target_Q = reward
             if not done:
@@ -263,6 +190,32 @@ class DRLmultiagent(object):
             result = self.main_network.fit(state.reshape(1, -1), current_qs, epochs=1, verbose=1)
 
             self.loss.append(result.history['loss'])
+        '''
+
+        states = np.zeros((batch_size, self.state_size))
+        target_qs_batch = np.zeros((batch_size, self.action_size))
+
+        # Loop over the minibatch and compute target Q values
+        for i, (state, action, reward, next_state, done, agent) in enumerate(minibatch):
+            target_Q = reward
+            if not done:
+                max_future_q = np.amax(
+                    getattr(self, f'target_network{agent + 1}').predict(next_state.reshape(1, -1))[0])
+                target_Q += self.gamma * max_future_q
+
+            current_qs = self.main_network.predict(state.reshape(1, -1))[0]
+            action_index = np.where(self.action_set == action)[0][0]
+            current_qs[action_index] = target_Q
+
+            # Store the state and the target Q values in their respective arrays
+            states[i] = state
+            target_qs_batch[i] = current_qs
+
+        # Train the main network in one go
+        result = self.main_network.fit(states, target_qs_batch, epochs=1, verbose=1)
+
+        # Append the average loss of this batch to the loss list
+        self.loss.append(np.mean(result.history['loss']))
 
 
 
