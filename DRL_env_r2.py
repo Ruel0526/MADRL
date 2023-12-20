16abedb0d91a62ba1bade2cfffa73bd18d73049c
bill304 <151612596+bill304@users.noreply.github.com>
2023-12-20 18:10:52 +0900

    Update DRL_env_r2.py

--- DRL_env_r2.py
+++ DRL_env_r2.py
@@ -7,6 +7,8 @@ import scipy.special as sp
 
 from sklearn.preprocessing import MinMaxScaler
 
+import itertools as it
+
 class DRLenv2(gym.Env):
     """
     Custom Environment simulating wireless communication with transmitters and users.
@@ -18,11 +20,22 @@ class DRLenv2(gym.Env):
         self.transmitters = 3
         self.users = 3
         self.TTI = 0
-        self.max_TTI = 10000 # Define your max TTI
-        self.pmax = math.pow(10, 3)
-        self.action_cand = 5
-        self.action_set = np.logspace(0, self.pmax, self.action_cand)
-        self.noise = math.pow(10, -10.4)
+        self.max_TTI = 50000 # Define your max TTI
+        #self.pmax = math.pow(10, 0.8)
+        #self.pmax = 1
+        self.action_cand = 10
+        self.pmin = 1
+        self.pmax = 1000
+        #self.action_set = np.linspace(0, self.pmax, self.action_cand)
+
+        min_power_dBm = 10 * np.log10(self.pmin)
+        max_power_dBm = 10 * np.log10(self.pmax)
+        step_size = (max_power_dBm - min_power_dBm) / 9
+        discrete_power_dBm = np.arange(min_power_dBm, max_power_dBm + step_size, step_size)
+        self.action_set = 10 ** (discrete_power_dBm / 10)
+
+
+        self.noise = math.pow(10, -10.4) #mW
         #self.state_size = 2 * (1 + self.transmitters) + self.transmitters * self.users * 2
         #self.state_size = (2+ self.users*self.transmitters)*2
         #self.state_size = (5 + self.users * self.transmitters + self.users * self.transmitters) * 2
@@ -32,8 +45,8 @@ class DRLenv2(gym.Env):
         self.T = 0.02
         self.rho = sp.jv(0, 2 * math.pi * self.f_d * self.T)
 
-        self.A = self.tx_positions_gen(self.transmitters, 100)
-        self.B, self.inside_status = self.rx_positions_gen(self.A, 99, 100)
+        self.A = self.tx_positions_gen(self.transmitters, 500)
+        self.B, self.inside_status = self.rx_positions_gen(self.A, 499, 500)
 
         # Define action and observation space
         self.action_space = spaces.Discrete(int(self.action_cand ** self.transmitters))
@@ -54,18 +67,59 @@ class DRLenv2(gym.Env):
         R = 100
         rx_positions = []
 
+        '''
         rx_positions.append((-R + 3 * math.cos(5 * math.pi / 6), -R / math.sqrt(3) + 3 * math.sin(5 * math.pi / 6)))
         rx_positions.append((-R+3*math.cos(math.pi/6), -R / math.sqrt(3)+3*math.sin(math.pi/6)))
         rx_positions.append((-R+3*math.cos(3*math.pi/2), -R / math.sqrt(3)+3*math.sin(3*math.pi/2)))
+
+
+        
+
+
+
+
         '''
+
         rx_positions.append((-2 * R+ (200/math.sqrt(3)-5) * math.cos(5 * math.pi / 6), 0+ (200/math.sqrt(3)-5) * math.sin(5 * math.pi / 6)))
         rx_positions.append((0+ (200/math.sqrt(3)-5) * math.cos(1 * math.pi / 6), 0+ (200/math.sqrt(3)-5) * math.sin(1 * math.pi / 6)))
         rx_positions.append((-R+ (200/math.sqrt(3)-5) * math.cos(3 * math.pi / 2), -R * math.sqrt(3) + (200/math.sqrt(3)-5) * math.sin(3 * math.pi / 2)))
-'''
+
+
+
+
+
+
+        '''
+        rx_positions.append((-2 * R + (90) * math.cos(0), 0 + (90) * math.sin(0)))
+        rx_positions.append((0 + (90) * math.cos(math.pi), 0 + (90) * math.sin(math.pi)))
+        rx_positions.append((-R+ (10) * math.cos(3 * math.pi / 2), -R * math.sqrt(3) + (10) * math.sin(3 * math.pi / 2)))
+
+
+        '''
+        
+        '''
+
+        rx_positions.append((-2 * R + (10) * math.cos(0), 0 + (10) * math.sin(0)))
+        rx_positions.append((0 + (10) * math.cos(0), 0 + (10) * math.sin(0)))
+        rx_positions.append(
+            (-R + (90) * math.cos(3 * math.pi / 2), -R * math.sqrt(3) + (90) * math.sin(3 * math.pi / 2)))
+        '''
+
+        '''
+        rx_positions.append((-2 * R + (30) * math.cos(0), 0 + (30) * math.sin(0)))
+        rx_positions.append((0 + (50) * math.cos(4 * math.pi / 3), 0 + (50) * math.sin(4 * math.pi / 3)))
+        rx_positions.append(
+            (-R + (70) * math.cos(2 * math.pi / 3), -R * math.sqrt(3) + (70) * math.sin(2 * math.pi / 3)))
+        '''
+
+
+
+
         self.B = rx_positions
 
-        self.H = np.ones((self.transmitters, self.transmitters)) * (
-                random.gauss(0, np.sqrt(1 / 2)) + random.gauss(0, np.sqrt(1 / 2)) * 1j)
+        #self.H = np.ones((self.transmitters, self.transmitters)) * (
+        #        random.gauss(0, np.sqrt(1 / 2)) + random.gauss(0, np.sqrt(1 / 2)) * 1j)
+        self.H = np.ones((self.transmitters, self.transmitters))
         self.channel_gain = np.zeros((self.transmitters, self.users))
         self.signal_powers = np.zeros((self.transmitters, self.users))
         for x in range(self.transmitters):
@@ -171,12 +225,13 @@ class DRLenv2(gym.Env):
         x2, y2 = rx_position
 
         d_k = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-        PL_0 = 140.7
+        PL_0 = 120.9
 
-        log_normal = 8
-        pathloss = PL_0 + 36.7 * math.log10(d_k / 1000) + np.random.normal(0, log_normal)
+        log_normal = 10
+        #pathloss = PL_0 + 37.6 * math.log10(d_k / 1000) + np.random.normal(0, log_normal)
+        pathloss = 140.7 + 36.7 * math.log10(d_k / 1000) #+ np.random.normal(0, log_normal)
 
-        gain = small_scale.conjugate() * small_scale / (10 ** (pathloss / 10))
+        gain = small_scale.conjugate() * small_scale*0.1/ (10 ** (pathloss / 10))
 
         return np.real(gain)
 
@@ -189,22 +244,26 @@ class DRLenv2(gym.Env):
             SINR = channel_gain[i, i] * actions[i] / (interferences + noise)
 
             # Cap the SINR at 30 dB
-            SINR = min(SINR, SINR_cap)
+            #SINR = min(SINR, SINR_cap)
             # if SINR == SINR_cap:
             # print('It is over 30dB')
 
-            sum_rate += math.log(1 + SINR)
+            sum_rate += math.log2(1 + SINR)
             sum_SINR += SINR
-        return sum_rate, sum_SINR
+        return sum_rate
 
     def decode_action(self, action):
-        power_levels = []
+        #power_levels = []
+        power_levels = np.zeros(self.transmitters)
         base = self.action_cand  # Assuming action_cand is the number of discrete levels per transmitter
 
+        i=0
         for _ in range(self.transmitters):
             level = action % base
-            power = level * (self.pmax / (base - 1))
-            power_levels.append(power)
+            #power = level * (self.pmax / (base - 1))
+            power = self.action_set[int(level)]
+            power_levels[i] = power
+            i+=1
             action //= base
 
         return power_levels
@@ -216,6 +275,8 @@ class DRLenv2(gym.Env):
         done = self.TTI >= self.max_TTI
         reward = 0
         info = {}
+        #print("A =", self.A)
+        #print("B =", self.B)
 
         old_state = np.copy(self.state)
         old_channel_gain = np.copy(self.channel_gain)
@@ -224,7 +285,9 @@ class DRLenv2(gym.Env):
         #actions = np.ones(self.transmitters) * self.action_set[action]
         actions = self.decode_action(action)
 
-        reward, sum_SINR = self.compute_sum_rate(self.channel_gain, actions, self.noise)
+        reward = self.compute_sum_rate(self.channel_gain, actions, self.noise)
+        best, guide_line = self.find_optimal_actions(self.channel_gain, self.action_set, self.noise, self.transmitters)
+
 
         for x in range(self.transmitters):
             for y in range(self.users):
@@ -236,6 +299,10 @@ class DRLenv2(gym.Env):
             for y in range(self.users):
                 self.channel_gain[x, y] = self.channel_gain_function(self.A[x], self.B[y], self.H[x, y])
                 self.signal_powers[x, y] = self.channel_gain[x, y] * actions[x]
+
+
+
+        #print("channel gain =" ,self.channel_gain)
 
         scaler = MinMaxScaler()
         gain_to_state = np.copy(self.channel_gain)
@@ -291,6 +358,8 @@ class DRLenv2(gym.Env):
         self.state[state_index + m * n + m * n + 5: state_index + m * n + m * n + 5 + m * n] = old_scaled_channel_gain
         self.state[state_index + m * n + m * n + 5 + m * n: state_index + m * n + m * n + 5 + m * n + m * n] = old_scaled_signal_powers
         '''
+
+        '''
         m, n = self.channel_gain.shape
         self.state[0] = action
         self.state[1] = actions[0]
@@ -298,7 +367,7 @@ class DRLenv2(gym.Env):
         self.state[3] = actions[2]
         self.state[4] = reward
         state_index = 5
-        self.state[state_index:state_index + m * n] = scaled_channel_gain
+        self.state[state_index:state_index + m * n] = self.channel_gain.flatten()
 
         self.state[state_index + m * n] = old_state[0]
         self.state[state_index + m * n+1] = old_state[1]
@@ -306,7 +375,30 @@ class DRLenv2(gym.Env):
         self.state[state_index + m * n+3] = old_state[3]
         self.state[state_index + m * n+4] = old_state[4]
 
-        self.state[state_index + m * n+5:] = old_scaled_channel_gain
+        self.state[state_index + m * n+5:] = old_channel_gain.flatten()
+        '''
+
+        scaled_power = []
+        for x in range(self.transmitters):
+            scaled_power.append(actions[x]/self.pmax)
+
+        m, n = self.channel_gain.shape
+        self.state[0] = action/(self.action_cand ** self.transmitters)
+        self.state[1] = scaled_power[0]
+        self.state[2] = scaled_power[1]
+        self.state[3] = scaled_power[2]
+
+        self.state[4] = reward#/guide_line
+        state_index = 5
+        self.state[state_index:state_index + m * n] = scaled_channel_gain
+
+        self.state[state_index + m * n] = old_state[0]
+        self.state[state_index + m * n + 1] = old_state[1]
+        self.state[state_index + m * n + 2] = old_state[2]
+        self.state[state_index + m * n + 3] = old_state[3]
+        self.state[state_index + m * n + 4] = old_state[4]
+
+        self.state[state_index + m * n + 5:] = old_scaled_channel_gain
 
 
 
@@ -314,3 +406,17 @@ class DRLenv2(gym.Env):
 
     # Define other necessary methods and functions
     # ...
+
+    def find_optimal_actions(self, channel_gain, action_set, noise, num_agents):
+
+        optimal_sum_rate = float('-inf')
+        best_actions = [0] * num_agents
+
+        # Iterate over all combinations of actions for each agent
+        for actions in it.product(action_set, repeat=num_agents):
+            current_sum_rate = self.compute_sum_rate(channel_gain, actions, noise)
+            if current_sum_rate > optimal_sum_rate:
+                optimal_sum_rate = current_sum_rate
+                best_actions = actions
+
+        return best_actions, optimal_sum_rate
